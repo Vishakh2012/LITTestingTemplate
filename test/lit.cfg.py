2d360482b44ad9a1b16d42e2fac3cbb6cb144581
Vishakh Prakash <vishakhpro2002@gmail.com>
2025-01-20 17:29:41 +0530

    Update lit.cfg.py

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -3,7 +3,7 @@ import lit.formats
 config.name = 'Test Suite'
 config.test_format = lit.formats.ShTest(True)
 
-config.suffixes = ['.mlir']
+config.suffixes = ['.ll'] #.mlir can also be used if need be
 
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.my_obj_root, 'test')
